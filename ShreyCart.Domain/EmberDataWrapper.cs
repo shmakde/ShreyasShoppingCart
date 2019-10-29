77d0b75e3b884e90c26a3687a43eb6e7a10da89b
Shreyas Makde <shreyas.makde@disa.com>
2019-10-29 14:49:47 -0500

    Ember Data Wrapper

---
+++ ShreyCart.Domain/EmberDataWrapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShreyCart.Domain
+{
+    public class EmberDataWrapper
+    {
+        public List<EmberProductWithTypeId> data { get; set; }
+    }
+}
